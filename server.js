51f0dbb93907ed411291fd178e7bf2d1ef631cea
Samarth Shukla <samritik2000@gmail.com>
2023-08-07 02:09:55 +0530

    updated to version 2

--- server.js
+++ server.js
@@ -28,11 +28,11 @@ app.use("/api/v1/analytics", require("./routes/analyticsRoutes"));
 app.use("/api/v1/admin", require("./routes/adminRoutes"));
 
 // STATIC FOLDER
-app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "./client2/build")));
 
 // STATIC ROUTES
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client2/build/index.html"));
 });
 
 //port
